132197a110ab8650dce32105d38127ddd4cf6cdd
florian <florian.toquin@gmail.com>
2024-03-20 23:53:35 +0100

    creation galery ajout css galery ajout resize et centrage img portrait ou paysage galery modification nom img colisee sans accent

---
+++ assets/script/resize-galery.js
@@ -0,0 +1,31 @@
+window.onload = () =>{
+
+ let listTopic = document.getElementById("galery-contain").getElementsByClassName("topic");
+
+ function resize(paramChildren){
+    for(let i=0; i<paramChildren.length; i++){
+        let currentElement = paramChildren[i];
+        let currentFigure = currentElement.getElementsByTagName("figure")[0];
+        let currentImg = currentElement.getElementsByTagName("img")[0];
+
+        console.log(currentFigure.offsetHeight +"  |   "+ currentImg.offsetHeight);
+        if(currentFigure.offsetHeight > currentImg.offsetHeight){
+            currentImg.style.width = "auto";
+            currentImg.style.height = "100%";
+            currentImg.style.top = "0px";
+            currentImg.style.left = ((currentFigure.offsetWidth - currentImg.offsetWidth)/2)+"px";
+        }else{
+            currentImg.style.width = "100%";
+            currentImg.style.height = "auto";
+            currentImg.style.left = "0px";
+            currentImg.style.top = ((currentFigure.offsetHeight - currentImg.offsetHeight)/2)+"px";
+        }
+    }
+ }
+
+ resize(listTopic);
+
+ window.onresize = () =>{
+    resize(listTopic);
+ }
+};
