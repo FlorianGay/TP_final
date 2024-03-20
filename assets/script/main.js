5e6657d3d33018f94b3e74f52363c12ba89d0bd1
Florian <floriangay750@gmail.com>
2024-03-20 18:13:06 +0100

    Création prettierrc index.html :     ajout polices écritures     ajout script     mise en page css header + footer + home     Modif header :         h1     création banner     création presentation     création last post     suppression class footer main.js :     Création function slider

---
+++ assets/script/main.js
@@ -0,0 +1,30 @@
+/* Slider */
+
+const slides = [
+    {
+        "image":"slide_1.jpg",
+        "alt":"Photo de barque dans un lac magnifique"
+    },
+    {
+        "image":"slide_2.jpg",
+        "alt":"Photo d'un pont en bois"
+    },
+    {
+        "image":"slide_3.jpg",
+        "alt":"Photo de champ de fleur devant une montagne enneigé"
+    }
+]
+
+let i = 0;
+
+function bannerSlide(index) {
+    document.querySelector(".banner-img").src=`../../assets/image/slideshow/${slides[index].image}`;
+    document.querySelector(".banner-img").setAttribute("alt", slides[index].alt);
+}
+
+function slideAuto() {
+    i = (i + 1) % slides.length;
+    bannerSlide(i)
+}
+
+setInterval(slideAuto, 5000)
