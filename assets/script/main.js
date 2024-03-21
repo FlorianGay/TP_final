c59f28f78aacf39d42f5de86ba672cd6c011ab55
Florian <floriangay750@gmail.com>
2024-03-21 10:17:17 +0100

    Responsive home mobile & tablet

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,30 +1,32 @@
 /* Slider */
 
 const slides = [
-    {
-        "image":"slide_1.jpg",
-        "alt":"Photo de barque dans un lac magnifique"
-    },
-    {
-        "image":"slide_2.jpg",
-        "alt":"Photo d'un pont en bois"
-    },
-    {
-        "image":"slide_3.jpg",
-        "alt":"Photo de champ de fleur devant une montagne enneigé"
-    }
+  {
+    image: 'slide_1.jpg',
+    alt: 'Photo de barque dans un lac magnifique',
+  },
+  {
+    image: 'slide_2.jpg',
+    alt: "Photo d'un pont en bois",
+  },
+  {
+    image: 'slide_3.jpg',
+    alt: 'Photo de champ de fleur devant une montagne enneigé',
+  },
 ]
 
-let i = 0;
+let i = 0
 
 function bannerSlide(index) {
-    document.querySelector(".banner-img").src=`../../assets/image/slideshow/${slides[index].image}`;
-    document.querySelector(".banner-img").setAttribute("alt", slides[index].alt);
+  document.querySelector(
+    '.banner-img'
+  ).src = `../../assets/image/slideshow/${slides[index].image}`
+  document.querySelector('.banner-img').setAttribute('alt', slides[index].alt)
 }
 
 function slideAuto() {
-    i = (i + 1) % slides.length;
-    bannerSlide(i)
+  i = (i + 1) % slides.length
+  bannerSlide(i)
 }
 
 setInterval(slideAuto, 5000)
