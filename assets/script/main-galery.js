31e46b60b9090b563f4b271984283a51c181b261
florian <florian.toquin@gmail.com>
2024-03-22 09:47:12 +0100

    ajout img petit
    ajout srcset galery

--- assets/script/main-galery.js
+++ assets/script/main-galery.js
@@ -1,3 +1,21 @@
 window.addEventListener("load", (event)=>{
     console.log("tata");
+
+    let galeryModal = document.getElementById("galery-modal");
+    let topics = document.getElementsByClassName("topic");
+
+    function openModal(pElement){
+
+    }
+
+    function addClickTopics(){
+        for(let i = 0; i < topics.length; i++){
+            
+        }
+            topics.addEventListener("click", function(pEvent){
+                console.log(pEvent);
+            });
+    }
+    addClickTopics();
+       
 })
