ef65bf0cc7d1220ca2372f379809081102ac9ea7
florian <florian.toquin@gmail.com>
2024-03-22 11:03:50 +0100

    modification lighthouse

--- assets/script/main-galery.js
+++ assets/script/main-galery.js
@@ -1,27 +1,32 @@
 window.addEventListener("load", (event)=>{
     let galeryModal = document.getElementById("galery-modal");
+    let galeryModalImg = document.getElementById("galery-modal-img");
     let close = document.getElementById("close");
     let prev = document.getElementById("prev");
     let next = document.getElementById("next");
     let topics = document.getElementsByClassName("topic");
 
+    function loadeImg(pSrc){
+        galeryModalImg.src=pSrc;
+    }
+
     function openModal(pElement){
-        console.log(galeryModal);
         galeryModal.style.display="block";
+        loadeImg(pElement.children[0].children[0].src);
     }
 
     function closeModal(){
-        
+        galeryModal.style.display="none";
     }
 
     function direction(pWay){
-
+        console.log(pWay);
     }
 
     function addEvent(){
         // events for topic
         for(let i = 0; i < topics.length; i++){
-            console.log(topics[i]);
+            // console.log(topics[i]);
             topics[i].addEventListener("click", function(pEvent){
                 pEvent.preventDefault();
                 openModal(topics[i]);
@@ -34,11 +39,12 @@ window.addEventListener("load", (event)=>{
         })
         prev.addEventListener("click", function(pEvent){
             pEvent.preventDefault();
+            direction(false);
         })
         next.addEventListener("click", function(pEvent){
             pEvent.preventDefault();
+            direction(true);
         })
     }
-    addEvent();
-       
+    addEvent();  
 })
